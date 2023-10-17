dd870320ea82e51f382d9588c80eec15b07eb101
chiZomDillibe <jonathanokorie2002@gmail.com>
2023-10-17 02:29:34 +0100

    modified main file

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -7,4 +5,4 @@ int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
-#endif
+
